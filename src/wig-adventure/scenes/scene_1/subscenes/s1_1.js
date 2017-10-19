0643aadcc45ec1f1b24b87f379793515cefc757f
Conrad Davis <conraddavis@google.com>
2017-10-18 17:26:40 -0700

    refactored to hard int

--- src/wig-adventure/scenes/scene_1/subscenes/s1_1.js
+++ src/wig-adventure/scenes/scene_1/subscenes/s1_1.js
@@ -1,20 +1,23 @@
-// build Scene One, section 1.1
+// Scene One, section 1.1
 
 import movePlayer from '../../../helpers/movePlayer';
 
-var s1_1 = (elements) => {
+var s1_1 = (elements, pageScrollPos) => {
 
-	// init timeline for the subscene
-	var s1_1 = new TimelineMax()
+	console.log('s1_1 CALLED')
 
 	// destructure the neccessary elements
 	const {
-		playerIdleHop,
+		playerIdleHover,
 		rowOneLandscape_S1,
 		rowTwoLandscape_S1,
 		rowThreeLandscape_S1,
-		playerIdleHopTl
+		playerIdleHoverTl,
+		s1_1_Tl
 	} = elements
+
+	console.log('pageScrollPos on the S1.1 LEVEL: ', pageScrollPos)
+	s1_1_Tl.pause()
 
 	// place scene Elements into an array to be passed to the movePlayer func
 	var sceneElements = [
@@ -24,20 +27,24 @@ var s1_1 = (elements) => {
 	]
 
 	// stop the player from jumping and make them jump in reverse direction
-	s1_1.call(() => playerIdleHop(false, elements))
-	s1_1.call(() => playerIdleHop(true, elements))
+	s1_1_Tl.call(() => playerIdleHover(false, elements))
+	s1_1_Tl.call(() => playerIdleHover(true, elements))
 	// simulate a double jump onto then off of the rock
 	// 1st jump
-	movePlayer(s1_1, sceneElements, 'right', 150, 0.5);
-	s1_1.to(player, 0.3, { x: 40, y: "-=250" }, "-=0.5")
-	s1_1.to(player, 0.2, { y: "+=50" })
+	// movePlayer(s1_1_Tl, sceneElements, 'right', 150, 0.5);
+	var distance = -540
+	s1_1_Tl.to(rowOneLandscape_S1, 0.5, { x: `${distance}` }), "-=0.5"
+	s1_1_Tl.to(rowTwoLandscape_S1, 0.5, { x: `${distance / 2}` }, "-=0.5")
+	s1_1_Tl.to(rowThreeLandscape_S1, 0.5, { x: `${distance / 4}` }, "-=0.5")
+	s1_1_Tl.to(player, 0.3, { x: 40, y: "-=250" }, "-=0.5")
+	s1_1_Tl.to(player, 0.2, { y: "+=50" })
 	// 2nd jump
-	movePlayer(s1_1, sceneElements, 'right', 190, 0.5);
-	s1_1.to(player, 0.3, { y: "-=50" }, "-=0.5")
-	s1_1.to(player, 0.2, { x: "-=40", y: 0 })
-	s1_1.call(() => playerIdleHop(false, elements))
+	// movePlayer(s1_1_Tl, sceneElements, 'right', 190, 0.5);
+	s1_1_Tl.to(player, 0.3, { y: "-=50" }, "-=0.5")
+	s1_1_Tl.to(player, 0.2, { y: 0 })
+	s1_1_Tl.call(() => playerIdleHover(false, elements))
 
-	return s1_1;
+	return s1_1_Tl;
 	
 }
 
--- src/wig-adventure/scenes/scene_1/subscenes/s1_2.js
+++ src/wig-adventure/scenes/scene_1/subscenes/s1_2.js
@@ -1,4 +1,4 @@
-// build Scene One, section 1.1
+// Scene One, section 1.2
 
 import movePlayer from '../../../helpers/movePlayer';
 
@@ -10,7 +10,7 @@ var s1_2 = (elements) => {
 	// destructure the neccessary elements
 	const {
 		playerBody,
-		playerIdleHop,
+		playerIdleHover,
 		rowOneLandscape_S1,
 		rowTwoLandscape_S1,
 		rowThreeLandscape_S1
@@ -24,28 +24,38 @@ var s1_2 = (elements) => {
 	]
 
 	// make the player walk throught the scene, up to the ledge
-	movePlayer(s1_2, sceneElements, 'right', 825, 20);
+	// movePlayer(s1_2, sceneElements, 'right', 825, 20);
+	var distance = -1655
+	s1_2.to(rowOneLandscape_S1, 5, { x: `${distance}` }), "-=5"
+	s1_2.to(rowTwoLandscape_S1, 5, { x: `${distance / 2}` }, "-=5")
+	s1_2.to(rowThreeLandscape_S1, 5, { x: `${distance / 4}` }, "-=5")
 	// make the player from bounce in reverse frame
-	s1_2.call(() => playerIdleHop(true, elements), null, null)
+	s1_2.call(() => playerIdleHover(true, elements), null, null)
 	// stop the player from bouncing
-	s1_2.call(() => playerIdleHop(true, elements), null, null)
+	s1_2.call(() => playerIdleHover(true, elements), null, null)
 	// tilt the player body downward
 	s1_2.to(playerBody, 5, {transformOrigin: 'center center', rotation: '390cw', y: 0})
 	// prolong the player's body looking downward
 	s1_2.to(playerBody, 8, {transformOrigin: 'center center', rotation: '390cw', y: 0})
 	// make the player bounce
-	s1_2.call(() => playerIdleHop(false, elements), null, null)
+	s1_2.call(() => playerIdleHover(false, elements), null, null)
 	// make the player walk to the left
-	movePlayer(s1_2, sceneElements, 'left', 280, 6);
+	// movePlayer(s1_2, sceneElements, 'left', 280, 6);
+	distance = -1385
+	s1_2.to(rowOneLandscape_S1, 3, { x: `${distance}` }), "-=3"
+	s1_2.to(rowTwoLandscape_S1, 3, { x: `${distance / 2}` }, "-=3")
+	s1_2.to(rowThreeLandscape_S1, 3, { x: `${distance / 4}` }, "-=3")
+	// tilt the player body back to an upward walking position
+	s1_2.to(playerBody, 1, {transformOrigin: 'center center', rotation: '360cw', y: 0}, "-=6")
 	// prolong the player's positiong
 	movePlayer(s1_2, sceneElements, 'left', 0, 5);
 	// player runs and jumps over the cliff
 	// run
-	movePlayer(s1_2, sceneElements, 'right', 620, 5);
+	movePlayer(s1_2, sceneElements, 'right', 600, 5);
 	// jump
-	s1_2.to(player, 1, { x: "-=90", y: "-=200" }, "-=4")
+	s1_2.to(player, 1, { y: "-=200" }, "-=4")
 	// land
-	s1_2.to(player, 1, { y: "+=70" }, "-=1.5")
+	s1_2.to(player, 1, { y: "+=50" }, "-=1.5")
 	return s1_2;
 	
 }
